aee6096704c6c3eb9216c20e519ededb29d21e20
caffeine <komalsnehal1991@gmail.com>
2015-11-18 15:37:18 -0500

    fixed checking if current website was visited

--- script/background.js
+++ script/background.js
@@ -3,12 +3,12 @@
         this.email = email;
         this.website = website;
     }
-    
+
     // define current user
     var user;
-    
+
     var fireBaseRef = new Firebase('https://leaksentry.firebaseio.com/users');
-    
+
     chrome.identity.getProfileUserInfo(function(userInfo){
         if(!userInfo.email){
             alert("Please log into your Google Account to use LeakSentry Chrome Extension.");
@@ -24,18 +24,15 @@
                     var email = userData.email;
                     var website;
                     if(userData.hasOwnProperty('website')){
-                        website = userData.website;    
+                        website = userData.website;
                     }
                     user = new User(email, website);
-                    
                     //console.log(user);
               });
-
             }
           });
         }
     });
-
 
     console.log('cookies');
     chrome.cookies.getAll({}, function(c){
@@ -52,7 +49,6 @@
           }
     });
 
-
     chrome.tabs.onActivated.addListener(function(tab){
           taburl = null;
           chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
@@ -65,7 +61,6 @@
              }
           );
     });
-
 
     chrome.tabs.onHighlighted.addListener(function(tab){
           taburl = null;
@@ -81,82 +76,44 @@
     });
 
     function getDomain(url){
-
       var a = document.createElement('a');
       a.href = url;
-
       return a.hostname;
     }
 
     chrome.webRequest.onBeforeSendHeaders.addListener(
       function(info) {
 
-        // console.log('taburl');
-        // console.log(taburl);
         if(taburl !== null && taburl !== undefined){
-
           var domain = getDomain(taburl);
-          /* moved to separate function
-          var url = taburl;
-          var text = taburl.split('/');
-          var new_text = text[2].split('.');
-          if(new_text){
-            var domain;
-            if(new_text.length < 3)
-              domain = new_text[0];
-            else
-              domain = new_text[1];
-          }*/
-
-          // console.log('taburl');
-          // console.log(taburl);
-          // console.log('new text');
-          // console.log(new_text);
-          // console.log('doamin');
-          // console.log(domain);
-
         }
 
         var url_thirdparty = info.url;
-        alert('Domain: ' + domain+ ' Third Party: '+ url_thirdparty + " "+ 
-url_thirdparty.indexOf(domain));
-          
-        if(url_thirdparty.indexOf(domain) != -1){
-          //console.log('same domain');
-            alert("True");
-        }
-        else{
-          //console.log('diff domain');
-          //console.log("intercepted: ");
-          //console.log(info);
-          //console.log(info.url);
+        if(!url_thirdparty.indexOf(domain) != -1){
+
+          console.log("Inspecting third party website " + url_thirdparty + " for possible PPI leak.");
 
           //parse the url using regex
           var regex = /[?]([^&#=]+)=([^&#=]+)/g;
           var found;
           var params = {};
-          alert(url_thirdparty + 'hey');    
           checkIfVisited(getDomain(url_thirdparty));
           while(found=regex.exec(url_thirdparty)){
             params[found[1]] = found[2];
             // Check if user visited the URL before
-            
-            // Get the response distribution for 
+            // Get the response distribution for
           }
 
           for(var param in params) {
-            //add here: go through the user's PII in firebase to check if the query parameters match any of the user provided PII and take required action 
+            //add here: go through the user's PII in firebase to check if the query parameters match any of the user provided PII and take required action
             console.log("Identified leak! The website " + domain + " is leaking your "+ param + " - " + params[param] + " to " + getDomain(url_thirdparty));
           }
         }
 
         // return {cancel:true};
-
         // Redirect the lolcal request to a random loldog URL.
         // var i = Math.round(Math.random() * loldogs.length);
         // return {redirectUrl: loldogs[i]};
-
-
       },
       // filters
       {
@@ -165,20 +122,22 @@ url_thirdparty.indexOf(domain));
       },
       // extraInfoSpec
       ["requestHeaders", "blocking"]);
-    
-// This function checks if URL was visited before by the user, if it was visited then returns action take otherwise returns
-// null
+
+      // This function checks if URL was visited before by the user, if it was visited then returns action take otherwise returns
     function checkIfVisited(url){
-        var urlModified = url.replace(".", "_dot_")
-        alert(url);
+
+        var re = /[\.]{1}/g;
+        urlModified = url.replace(re, '_dot_');
+
         // if user is undefined return null - not possible though but shit can happen
         if(typeof user == "undefined")
             return null;
-        
-        if(typeof user.website == undefined){
+
+        if(typeof user.website == "undefined"){
             return null;
         }
-        if(user.website.hasOwnProperty(url))
-            alert(user.website[url]);
-            console.log('website found' + url);
+
+        var website = user.website;
+        if(website.hasOwnProperty(urlModified))
+            console.log('website found' + url + " " + website[urlModified]);
     }
